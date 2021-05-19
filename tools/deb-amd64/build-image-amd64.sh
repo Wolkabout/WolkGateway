d6f0fe6814348e7a169fc616f05541108131081c
Danilo Ješić <34022788+djesic@users.noreply.github.com>
2021-05-19 14:49:33 +0200

    Modified scripts (#73)

--- tools/deb-amd64/build-image-amd64.sh
+++ tools/deb-amd64/build-image-amd64.sh
@@ -15,12 +15,8 @@
 #  limitations under the License.
 #
 
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run as sudo."
-  exit
-fi
-
 cp ../make_deb.sh .
+cp ../*.zip .
 
 docker build -t wolkabout:wg-amd64 .
 rm make_deb.sh

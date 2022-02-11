7ca17340c8a0d054ab8679b1f42ad58a003be1a9
nanavuletic <ana.vuletic@wolkabout.com>
2022-02-11 16:02:43 +0100

    Changed the `make_zip.sh` to `--init --recursive` the submodules

--- tools/make_zip.sh
+++ tools/make_zip.sh
@@ -45,11 +45,7 @@ if [ $? -ne 0 ]; then
   echo "Can't checkout to branch named $branch"
   exit
 fi
-git submodule update --recursive
-cd ./WolkSDK-Cpp || exit
-git submodule init
-git submodule update
-cd .. || exit
+git submodule update --init --recursive
 filename="WolkGateway-v$(cat RELEASE_NOTES.txt | grep "**Version" | head -1 | sed -e "s/**Version //" | sed -e "s/\*\*//").zip"
 echo "filename: $filename"
 zip -qr $filename *
